bd7434d7c9b29ab50ef6540a58179d6c122f1e12
chintuistarla <167430817+chintuistarla@users.noreply.github.com>
2024-12-07 12:43:21 +0530

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Order = require('../models/OrderModel');
 
-// Create Order
+// Creating Order
 router.post('/', async (req, res) => {
     try {
         const orderData = {
