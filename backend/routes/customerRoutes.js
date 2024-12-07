eaef0d504457153138f25195c655320e55622deb
chintuistarla <167430817+chintuistarla@users.noreply.github.com>
2024-12-07 12:43:07 +0530

    Update customerRoutes.js

--- backend/routes/customerRoutes.js
+++ backend/routes/customerRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Customer = require('../models/CustomerModel');
 
-// Create Customer
+// Creating  Customer
 router.post('/', async (req, res) => {
     console.log("Request Body: ", req.body); // Log request body
 
